ba09a6db5a6145df78092d77bf0f862002502f8b
couenzo <enzo.courvalet@ynov.com>
2025-09-15 15:03:01 +0200

    init_choise_team

--- src/auth/character.go
+++ src/auth/character.go
@@ -1,6 +1,10 @@
 package auth
 
-import "fmt"
+import (
+	"fmt"
+	"projet-red_from-boots-to-brass/src/utils"
+	"time"
+)
 
 type Character struct { // create class character
 	name   string
@@ -12,37 +16,51 @@ type Character struct { // create class character
 	bag    []string
 }
 
+//type Skill struct { // create class Skill
+//	name_wp string
+//	pt_dam  int
+//}
 
-
-type Skill struct { // create class Skill
-	name_wp string
-	pt_dam  int
-}
-
-type Equipment struct { // create class equipement
-	boots string
-	pants string
-	shoes string
-}
+//type Equipment struct { // create class equipement
+//boots string
+//pants string
+//shoes string
+//}
 
 func InitCharacter() {
-	fmt.Println("hello world !") //test
-	p1 := Character{"soldat_FR", "France", 0, 100, 100, 0, []string{"vide", "vide", "vide", "vide", "vide"}}
-    p2 := Character{"soldat_US",   "U.S.A",  0,  100,  100, 0, []string{"vide", "vide", "vide", "vide", "vide"}}
-    p3 := Character{"soldat_RU",   "Vert",  0, 100, 100, 0, []string{"vide", "vide", "vide", "vide", "vide"}}
-	characters := []Character{p1, p2, p3}
-	for _, c := range characters {
-    fmt.Printf("%s (team %s) niveau %d\n", c.name, c.team, c.level)
+	var c Character
+	p1 := Character{"soldat_FR", "France", 0, 100, 100, 0, []string{"FAMAS", "vide", "vide", "vide", "vide"}}
+	p2 := Character{"soldat_US", "U.S.A", 0, 100, 100, 0, []string{"M16", "vide", "vide", "vide", "vide"}}
+	p3 := Character{"soldat_RU", "U.R.S.S", 0, 100, 100, 0, []string{"PPSH", "vide", "vide", "vide", "vide"}}
+	for {
+		fmt.Println("Choisi ton camp:\n1-France\n2-U.S.A\n3-U.R.S.S")
+		fmt.Scan(&c.team)
+		if c.team == "France" {
+			fmt.Print(p1)
+			break
+		} else if c.team == "U.S.A" {
+			fmt.Print(p2)
+			break
+		} else if c.team == "soldat_RU" {
+			fmt.Print(p3)
+			break
+		} else {
+			fmt.Print("Choisissez une team valide.")
+			time.Sleep(3 * time.Second)
+			utils.Clear()
+			continue
+		}
+	}
 }
 
-func displayinfo() {
+//func displayinfo() {
 
-}
+//}
 
-func characterCreation() {
+//func characterCreation() {
 
-}
+//}
 
-func upgradeInventorySlot() {
+//func upgradeInventorySlot() {
 
-}
+//}
