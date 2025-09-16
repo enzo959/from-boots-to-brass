d8c6f24294811b1d48b5e2b3c011abf1bc4127e4
couenzo <enzo.courvalet@ynov.com>
2025-09-16 23:19:21 +0200

    use_func_initCharacter

--- main.go
+++ main.go
@@ -2,15 +2,14 @@ package main
 
 import (
 	"projet-red_from-boots-to-brass/src/auth"
-	"projet-red_from-boots-to-brass/src/utils"
-	"time"
 )
 
 func main() {
-	p1, p2, p3 := auth.Displayinfo()
+	/*p1, p2, p3 := auth.DisplayCharacter()
 	auth.CharacterCreation(p1, p2, p3) // start func InitCharacter
 	time.Sleep(10 * time.Second)       // stop the programme during 5s
 	utils.Clear()
 	t1, t2, t3 := auth.Book() // start func Clear
-	auth.Menu(t1, t2, t3)
+	auth.Menu(t1, t2, t3)*/
+	auth.InitCharacter()
 }
