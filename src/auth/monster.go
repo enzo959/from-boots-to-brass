08c1e9dd2d95c8e127173c24494496d99a583b0e
couenzo <enzo.courvalet@ynov.com>
2025-09-16 16:36:11 +0200

    end_day

--- src/auth/monster.go
+++ src/auth/monster.go
@@ -1,7 +1,5 @@
 package auth
 
-import "fmt"
-
 type Monster struct {
 	name   string
 	max_lp int
@@ -9,39 +7,12 @@ type Monster struct {
 	att_pt int
 }
 
-func initGobelin() {
-	var m1 Monster // initialisation de ma structure Personnage
-
-	/*
-	   Initialisation des attributs de mon personnage p1
-	*/
-	m1.name = "Training gobelin"
-	m1.max_lp = 40
-	m1.cur_lp = m1.max_lp
-	m1.att_pt = 5
-
-	fmt.Println("Monster LP", m1.name, ":", m1.max_lp)
-	fmt.Println("Monster strength", m1.name, ":", m1.att_pt)
-
-	//if m1.mort {
-	//  fmt.Println("Vie du personnage", p1.nom, "est mort")
-	// } else {
-	//     fmt.Println("Vie du personnage", p1.nom, "est vivant")
-	// }
-}
-
-func initMonster() (Monster, Monster, Monster, Monster, Monster) {
-
-	m2 := Monster{"M2", 100, 100, 2}
-	m3 := Monster{"M3", 80, 100, 3}
-	m4 := Monster{"M4", 80, 100, 2}
-	m5 := Monster{"M5", 70, 100, 2}
-	m6 := Monster{"M6", 100, 100, 10}
+func InitMonster() (Monster, Monster, Monster, Monster, Monster) {
+	m1 := Monster{"Training gobelin", 100, 100, 1}
+	m2 := Monster{"M2", 100, 100, 1}
+	m3 := Monster{"M3", 80, 100, 1}
+	m4 := Monster{"M4", 80, 100, 1}
+	m5 := Monster{"M5", 70, 100, 1}
+	m6 := Monster{"M6", 100, 100, 1}
 	return m2, m3, m4, m5, m6
-
-	//if m.Mort {
-	//   fmt.Println("Vie du personnage", p.Nom, "est mort")
-	// } else {
-	//   fmt.Println("Vie du personnage", p.Nom, "est vivant")
-	// }
 }
