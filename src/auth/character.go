f8a4308724dc492002e5bfa396ae82b5fb2a8de9
couenzo <enzo.courvalet@ynov.com>
2025-09-16 14:26:37 +0200

    init_func_Menu_completed

--- src/auth/character.go
+++ src/auth/character.go
@@ -27,48 +27,51 @@ type Character struct { // create class character
 //shoes string
 //}
 
-func InitCharacter() (Character, Character, Character) {
+//func InitCharacter() (Character, Character, Character) {
+//p1 := Character{"soldat_FR", "France", 0, 100, 100, 0, []string{"FAMAS", "vide", "vide", "vide", "vide"}} // var p1
+//p2 := Character{"soldat_US", "U.S.A", 0, 100, 100, 0, []string{"M16", "vide", "vide", "vide", "vide"}}
+//p3 := Character{"soldat_RU", "U.R.S.S", 0, 100, 100, 0, []string{"PPSH", "vide", "vide", "vide", "vide"}}
+//return p1, p2, p3
+//}
+
+func Displayinfo() (Character, Character, Character) {
 	p1 := Character{"soldat_FR", "France", 0, 100, 100, 0, []string{"FAMAS", "vide", "vide", "vide", "vide"}} // var p1
 	p2 := Character{"soldat_US", "U.S.A", 0, 100, 100, 0, []string{"M16", "vide", "vide", "vide", "vide"}}
 	p3 := Character{"soldat_RU", "U.R.S.S", 0, 100, 100, 0, []string{"PPSH", "vide", "vide", "vide", "vide"}}
 	return p1, p2, p3
 }
 
-//func displayinfo() {
-
-//}
-
 func CharacterCreation(p1, p2, p3 Character) Character {
-	var choice string
+	var choiceName string
 	var perso Character
 
 	for {
 		fmt.Println("Choisissez votre camp :\n1 - France\n2 - U.S.A\n3 - U.R.S.S")
-		fmt.Scan(&choice)
+		fmt.Scan(&choiceName)
 
-		switch choice {
-		case "1", "France":
-			perso = p1
+		switch choiceName {
+		case "1", "France": // if user write "1" or "France"
+			perso = p1 // perso = p1
 		case "2", "U.S.A":
 			perso = p2
 		case "3", "U.R.S.S":
 			perso = p3
 		default:
-			fmt.Println("Choisissez une option valide.")
-			time.Sleep(2 * time.Second)
-			utils.Clear()
-			continue
+			fmt.Println("Choisissez une option valide.") // error gestion
+			time.Sleep(2 * time.Second)                  // reset after 2 sec
+			utils.Clear()                                // clean terminal
+			continue                                     // restart the code
 		}
-		break
+		break // break the code
 	}
 
 	fmt.Println("entre ton nom soldat")
 	var newName string
 	fmt.Scan(&newName)
-	perso.name = newName
+	perso.name = newName // attribut new name for perso
 
 	fmt.Printf("\nVous êtes : %s\n", perso.name)
-	fmt.Printf("Team : %s | Niveau : %d | PV : %d/%d | Argent : %d\n",
+	fmt.Printf("Team : %s | Niveau : %d | PV : %d/%d | Argent : %d\n", //layout
 		perso.team, perso.level, perso.pv_act, perso.pv_max, perso.money)
 	fmt.Printf("Équipement : %v\n", perso.bag)
 
