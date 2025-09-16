e0aa7d431af61108385c70169f4b4b6cbc6df73d
couenzo <enzo.courvalet@ynov.com>
2025-09-16 23:23:58 +0200

    func_displayMonster_creat_struct_Monster_creat

--- src/auth/monster.go
+++ src/auth/monster.go
@@ -7,12 +7,12 @@ type Monster struct {
 	att_pt int
 }
 
-func InitMonster() (Monster, Monster, Monster, Monster, Monster) {
+func displayMonster() (Monster, Monster, Monster, Monster, Monster, Monster) {
 	m1 := Monster{"Training gobelin", 100, 100, 1}
 	m2 := Monster{"M2", 100, 100, 1}
 	m3 := Monster{"M3", 80, 100, 1}
 	m4 := Monster{"M4", 80, 100, 1}
 	m5 := Monster{"M5", 70, 100, 1}
 	m6 := Monster{"M6", 100, 100, 1}
-	return m2, m3, m4, m5, m6
+	return m1, m2, m3, m4, m5, m6
 }
