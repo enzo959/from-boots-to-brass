9e1f22b1fd879885bed8718680652b8656e49e20
couenzo <enzo.courvalet@ynov.com>
2025-09-17 14:02:19 +0200

    complete_func_Fight

--- src/game/play.go
+++ src/game/play.go
@@ -1,11 +1,37 @@
 package game
 
-type Skill struct { // create class Skill
-	name_wp string
-	pt_dam  int
+import (
+	"fmt"
+	"projet-red_from-boots-to-brass/src/utils"
+)
+
+type Text struct {
+	text string
 }
 
-func displaySkills() Skill {
-	p1 := Skill{"test", 10}
-	return p1
+func Fight(b1, b2 Text) {
+	var choiceOption string
+	var option Text
+
+	for {
+		fmt.Println("Menu:\n1 - Rêgles\n2 - Combat\n3 - Learn\n4 - Marchand\nPour quitter le jeu lisez le prologue ;)")
+		fmt.Scan(&choiceOption)
+
+		switch choiceOption {
+		case "1", "Rêgles":
+			fmt.Print(b1)
+		case "2", "Combat":
+			fmt.Println(b2)
+		case "3", "Learn":
+			l1, l2 := Learn()
+			fmt.Println(l1, l2)
+		case "4", "Marchand":
+			w1, w2, w3, e1, e2, e3, l1, l2 := Update()
+			fmt.Println(w1, w2, w3, e1, e2, e3, l1, l2)
+		default:
+			utils.Invalid()
+		}
+		utils.Clear()
+		fmt.Printf("\n%s\n", option.text)
+	}
 }
