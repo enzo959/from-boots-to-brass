49614f4dd2f6a6621f360224d9d77a61b3350581
couenzo <enzo.courvalet@ynov.com>
2025-09-18 15:13:57 +0200

    edit_case_on_func_Fight_for_go_Marchand

--- src/auth/play.go
+++ src/auth/play.go
@@ -6,32 +6,37 @@ import (
 
 func Fight(t3, t4 Text) {
 	var choiceOption string
-
+	var perso *Character
 	for {
-		fmt.Println("Menu de jeu:\n1 - Rêgles\n2 - Crée son personage\n3 - Combat\n4 - Learn\n5 - Marchand\n6 - Allé au Menu Principal")
-		fmt.Scan(&choiceOption)
+		fmt.Println("Menu de jeu:\n1 - Rêgles\n2 - Crée son personage\n3 - Combat\n4 - Learn\n5 - Marchand\n6 - Allé au Menu Principal\nPour quitter le jeu allez au Menu Principal ;)")
+		fmt.Scanln(&choiceOption)
 
 		switch choiceOption {
 		case "1", "Rêgles":
 			fmt.Print(t3)
 			Back()
 		case "2", "Crée son personage":
-			InitCharacter()
+			p := InitCharacter()
+			perso = &p
+			fmt.Printf("Personnage créé: %+v\n", p)
+			Back()
 		case "3", "Combat":
 			t5 := Book3()
 			Battle(t5)
 		case "4", "Learn":
 			l1, l2 := Learn()
-			fmt.Println(l1, l2)
+			fmt.Printf("%v\n%v\n", l1, l2)
 			Back()
 		case "5", "Marchand":
-			w1, w2, w3, e1, e2, e3, l1, l2 := Update()
-			fmt.Println(w1, w2, w3, e1, e2, e3, l1, l2)
+			if !CheckPerso(perso) {
+				break
+			}
+			Marchand(perso)
 			Back()
 		case "6", "Allé au Menu Principal":
 			t6 := Book4()
 			fmt.Println(t6)
-			BackMenuPrincipal()
+			return
 		default:
 			Invalid()
 		}
@@ -43,13 +48,13 @@ func Battle(t5 Text) {
 
 	for {
 		fmt.Println("Action de jeu:\n1 - Attaque\n2 - Fuite")
-		fmt.Scan(&choiceOption)
+		fmt.Scanln(&choiceOption)
 
 		switch choiceOption {
 		case "1", "Attaque":
 			fmt.Print(t5)
 		case "2", "Fuite":
-			BackMenuJeu()
+			Back()
 		}
 	}
 }
