fbe8e4ff404c8e5e3eddc414ad483d00ee33118b
couenzo <enzo.courvalet@ynov.com>
2025-09-18 20:50:32 +0200

    separated_func_useItem_on_func_chooseItemIndex_applyWeapon_applyHeal_and_separated_func_Battle_on_func_monsterAttack_playerAttack_askPlayer_displayStatus

--- src/auth/play.go
+++ src/auth/play.go
@@ -2,6 +2,7 @@ package auth
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func Fight(t3, t4 Text) {
@@ -22,7 +23,11 @@ func Fight(t3, t4 Text) {
 			Back()
 		case "3", "Combat":
 			t5 := Book3()
-			Battle(t5)
+			if !CheckPerso(perso) {
+				break
+			}
+			Battle(t5, perso)
+			Back()
 		case "4", "Learn":
 			l1, l2 := Learn()
 			fmt.Printf("%v\n%v\n", l1, l2)
@@ -43,18 +48,129 @@ func Fight(t3, t4 Text) {
 	}
 }
 
-func Battle(t5 Text) {
-	var choiceOption string
+// Affiche l'inventaire et renvoie l'index choisi (ou -1 si annuler)
+func chooseItemIndex(bag []Item) int {
+	if len(bag) == 0 {
+		fmt.Println("Inventaire vide.")
+		return -1
+	}
+
+	fmt.Println("Inventaire :")
+	for i, it := range bag {
+		fmt.Printf("%d - %s (%s, puissance %d)\n", i+1, it.Name, it.Kind, it.Power)
+	}
+	fmt.Print("Choisissez un objet (0 pour annuler) : ")
+
+	var n int
+	fmt.Scanln(&n)
+	if n <= 0 || n > len(bag) {
+		return -1
+	}
+	return n - 1
+}
+
+func applyWeapon(item Item, monster *Monster) {
+	min := item.Power / 2
+	dmg := rand.Intn(item.Power-min+1) + min
+	monster.CurLP -= dmg
+	fmt.Printf("Vous utilisez %s et infligez %d dégâts !\n", item.Name, dmg)
+}
+
+func applyHeal(item Item, perso *Character) bool {
+	if perso.PvAct >= perso.PvMax-item.Power {
+		fmt.Println("Vos PV sont trop élevés pour utiliser cet objet.")
+		return false
+	}
+	gain := item.Power
+	if perso.PvAct+gain > perso.PvMax {
+		gain = perso.PvMax - perso.PvAct
+	}
+	perso.PvAct += gain
+	fmt.Printf("Vous utilisez %s et récupérez %d PV !\n", item.Name, gain)
+	return true // consommé
+}
+
+func useItem(perso *Character, monster *Monster) {
+	idx := chooseItemIndex(perso.Bag)
+	if idx < 0 {
+		return
+	}
+
+	item := perso.Bag[idx]
+	switch item.Kind {
+	case "weapon":
+		applyWeapon(item, monster)
+		// retirer si arme consommable :
+		// perso.Bag = append(perso.Bag[:idx], perso.Bag[idx+1:]...)
+
+	case "heal":
+		if applyHeal(item, perso) {
+			// soin consommé
+			perso.Bag = append(perso.Bag[:idx], perso.Bag[idx+1:]...)
+		}
+
+	default:
+		fmt.Println("Objet inconnu.")
+	}
+}
+
+func displayStatus(p *Character, m *Monster) {
+	fmt.Printf("\n%s : %d/%d PV | %s : %d/%d PV\n",
+		p.Name, p.PvAct, p.PvMax,
+		m.Name, m.CurLP, m.MaxLP)
+}
+
+func askPlayerAction() string {
+	fmt.Println("Actions :\n1 - Attaque basique\n2 - Utiliser objet\n3 - Fuite")
+	var choice string
+	fmt.Scanln(&choice)
+	return choice
+}
+
+func playerAttack(p *Character, m *Monster) {
+	base := rand.Intn(21) + 10 // dégâts aléatoires 10..30
+	dmg := base + p.Level*2    // option : bonus lié au level
+	m.CurLP -= dmg
+	fmt.Printf("%s attaque et inflige %d dégâts !\n", p.Name, dmg)
+}
+
+func monsterAttack(p *Character, m *Monster) {
+	dmg := rand.Intn(m.AttPt + 1)
+	p.PvAct -= dmg
+	fmt.Printf("%s attaque et inflige %d dégâts !\n", m.Name, dmg)
+}
+
+func Battle(t5 Text, perso *Character) {
+	fmt.Println("⚔️  Un combat commence !")
+	monster := PickRandomMonster()
+	fmt.Printf("Un %s sauvage apparaît avec %d PV !\n", monster.Name, monster.CurLP)
 
 	for {
-		fmt.Println("Action de jeu:\n1 - Attaque\n2 - Fuite")
-		fmt.Scanln(&choiceOption)
+		displayStatus(perso, &monster)
 
-		switch choiceOption {
-		case "1", "Attaque":
-			fmt.Print(t5)
-		case "2", "Fuite":
-			Back()
+		switch askPlayerAction() {
+		case "1":
+			playerAttack(perso, &monster)
+		case "2":
+			useItem(perso, &monster)
+		case "3":
+			fmt.Println("Vous prenez la fuite !")
+			return
+		default:
+			fmt.Println("Choix invalide.")
+			continue
+		}
+
+		if monster.CurLP <= 0 {
+			fmt.Printf("🎉 Vous avez vaincu %s !\n", monster.Name)
+			return
+		}
+
+		monsterAttack(perso, &monster)
+
+		if perso.PvAct <= 0 {
+			fmt.Println("💀 Vous êtes K.O. !")
+			return
 		}
 	}
 }
