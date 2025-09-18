59c7f9e37ecc267d24f283578347fb6ea0e949a3
couenzo <enzo.courvalet@ynov.com>
2025-09-18 23:48:32 +0200

    change_language

--- src/auth/utils.go
+++ src/auth/utils.go
@@ -10,24 +10,24 @@ type Text struct {
 }
 
 func Book2() (Text, Text) {
-	t3 := Text{"Rêgles: Vous pouvez faire des combat dans 'Combat'.\nVous pouvez apprendre de nouvelle technique de combat dans 'Learn'.\nVous pouvez aller faire des achat dans 'Marchand'"}
-	t4 := Text{"Combat: vous êtes dans l'arène."}
+	t3 := Text{"Rules: You can participate in great battles against great dictators. You can learn new combat tactics. You can increase your artillery."}
+	t4 := Text{"You are entering a war zone."}
 	return t3, t4
 }
 
 func Book3() Text {
-	t5 := Text{"Menu Attack"}
+	t5 := Text{"Attack Menu"}
 	return t5
 }
 
 func Book4() Text {
-	t6 := Text{"Retour au Menu"}
+	t6 := Text{"Back to Menu"}
 	return t6
 }
 
 func Book1() (Text, Text) {
-	t1 := Text{"Prologue: \n Dans ce monde, la gloire n’est pas donnée. Elle se prend, balle après balle. \n Je ne suis qu’un soldat parmi des milliers : un matricule, une arme, un ordre. \n On ne choisit pas où l’on commence. On choisit jusqu’où on ira. \n Les tirs résonnent au loin, les hélicoptères déchirent le ciel. Aujourd’hui, ce n’est plus qu’une mission : c’est le premier pas. \n Le champ de bataille forge les hommes… et je compte bien gravir chaque échelon, quel qu’en soit le prix."}
-	t2 := Text{"Rêgle: \n Vous pouvez faire des combats dans Game.\nVous pouvez visité le Marchant, qui vous permet de faire des achats."}
+	t1 := Text{"Prologue: \n In this world, glory is not given. It is taken, bullet by bullet. \n I am just one soldier among thousands: a serial number, a weapon, an order. \n We don't choose where we start. We choose how far we will go. Gunfire echoes in the distance, helicopters tear through the sky. Today, it's no longer just a mission: it's the first step. The battlefield forges men... and I intend to climb every rung of the ladder, whatever the cost."}
+	t2 := Text{"Rules: \n You can engage in legendary battles. You can upgrade your artillery."}
 	return t1, t2
 }
 
@@ -36,20 +36,20 @@ func Clear() { // func clean terminal
 }
 
 func Invalid() {
-	fmt.Println("Choisissez une option valide.") // error gestion
-	time.Sleep(2 * time.Second)                  // reset after 2 sec
-	Clear()                                      // clean terminal
+	fmt.Println("Select a valid option.") // error gestion
+	time.Sleep(2 * time.Second)           // reset after 2 sec
+	Clear()                               // clean terminal
 }
 
 func Back() {
 	var back string // var back for back to menu
-	fmt.Println("\nAppuyez sur 'q' pour revenir en arrière")
+	fmt.Println("\nPress ‘q’ to confirm")
 	fmt.Scanln(&back)
 }
 
 func CheckPerso(perso *Character) bool {
 	if perso == nil {
-		fmt.Println("Créez d’abord un personnage pour acheter.")
+		fmt.Println("First, create a character to purchase weapons.")
 		Back()
 		return false
 	}
