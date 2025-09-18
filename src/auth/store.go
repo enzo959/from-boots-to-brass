7143659875ae5fefae7d249a6893c638f884a71f
couenzo <enzo.courvalet@ynov.com>
2025-09-18 15:48:58 +0200

    ajout_struct_StoreItem

---
+++ src/auth/store.go
@@ -0,0 +1,39 @@
+package auth
+
+type Loot struct {
+	Name      string
+	InfoObjet string
+	Pv        int
+	InfoPv    string
+	Price     int
+	InfoPrice string
+}
+
+type Weapons struct { //create struct update
+	Name       string
+	InfoName   string
+	PvAttack   int
+	InfoAttack string
+	Price      int
+	InfoPrice  string
+}
+
+type Equipment struct { // create struct equipement
+	Name      string
+	InfoName  string
+	PvSup     int
+	InfoPvSup string
+	Price     int
+	InfoPrice string
+}
+
+type Skill struct { // create struct Skill
+	Name          string
+	PtAttack      int
+	LevelRequiert int
+}
+
+type StoreItem struct { // create struct StoreItem
+	Name  string
+	Price int
+}
