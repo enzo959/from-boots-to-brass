6f4c8333051dab8b7b14566996f17510326ab0cf
couenzo <enzo.courvalet@ynov.com>
2025-09-18 23:46:27 +0200

    change_language

--- src/auth/marchand.go
+++ src/auth/marchand.go
@@ -7,14 +7,14 @@ import (
 func Marchand(perso *Character) {
 	var choiceOption string
 	for {
-		fmt.Println("Vous etes chez le marchand\n1 - Voir les Objet en vente\n2 - Allé au Menu de jeu")
+		fmt.Println("You are at the store\n1 - See Items for Sale\n2 - Go to Game Menu")
 		fmt.Scanln(&choiceOption)
 
 		switch choiceOption {
-		case "1", "Voir les Objet en Vente":
+		case "1", "See Items for Sale":
 			BuyItem(perso)
 			Back()
-		case "2", "Allé au Menu de jeu":
+		case "2", "Go to Game Menu":
 			return
 		default:
 			Invalid()
@@ -25,7 +25,7 @@ func Marchand(perso *Character) {
 func BuyItem(perso *Character) {
 	w1, w2, w3, e1, e2, e3, l1, l2 := Update()
 
-	// Construis une slice d'Item (nom, type, puissance, prix)
+	// Build an Item slice
 	store := []Item{
 		{w1.Name, "weapon", w1.PvAttack, w1.Price},
 		{w2.Name, "weapon", w2.PvAttack, w2.Price},
@@ -34,16 +34,16 @@ func BuyItem(perso *Character) {
 		{e2.Name, "heal", e2.PvSup, e2.Price},
 		{e3.Name, "heal", e3.PvSup, e3.Price},
 		{l1.Name, "heal", l1.PvAttack, l1.Price},
-		{l2.Name, "weapon", l2.PvAttack, l2.Price}, // adapte si l2 est loot/weapon
+		{l2.Name, "weapon", l2.PvAttack, l2.Price}, // fits between loot/weapon
 	}
 
-	fmt.Println("Objets disponibles :")
+	fmt.Println("Available items :")
 	for i, it := range store {
-		fmt.Printf("%d - %s (%s) — Prix : %d\n", i+1, it.Name, it.Kind, it.Price)
+		fmt.Printf("%d - %s (%s) — Price : %d\n", i+1, it.Name, it.Kind, it.Price)
 	}
 
-	fmt.Printf("\nVous avez %d pièces.\n", perso.Money)
-	fmt.Print("Entrez le numéro de l’objet à acheter, si vous ne voulez rien entrez 0: ")
+	fmt.Printf("\nYou have %d credit.\n", perso.Money)
+	fmt.Print("Enter the number of the item you want to purchase. If you do not want to purchase anything, enter 0: ")
 	var num int
 	fmt.Scanln(&num)
 
@@ -51,15 +51,15 @@ func BuyItem(perso *Character) {
 		chosen := store[num-1]
 
 		if perso.Money < chosen.Price {
-			fmt.Printf("Pas assez d'argent pour acheter %s (prix %d).\n", chosen.Name, chosen.Price)
+			fmt.Printf("Not enough credit to buy %s (price %d).\n", chosen.Name, chosen.Price)
 			return
 		}
 
-		// retire l'argent et ajoute l'objet (Item) dans l'inventaire
+		// removes the credit and adds Item to the inventory
 		perso.Money -= chosen.Price
 		perso.Bag = append(perso.Bag, chosen)
 
-		fmt.Printf("%s a été ajouté dans votre inventaire ;)\n", chosen.Name)
+		fmt.Printf("%s has been added to your inventory ;)\n", chosen.Name)
 		AccessInventory(perso)
 	}
 }
